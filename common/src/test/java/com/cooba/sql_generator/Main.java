60750476c84c71b9f69e0cf4aee39733dbdbb6a8
Aiden <aiden.lin@ambernet.com.tw>
2024-04-22 16:27:11 +0800

    add sql

--- common/src/test/java/com/cooba/sql_generator/Main.java
+++ common/src/test/java/com/cooba/sql_generator/Main.java
@@ -1,10 +1,12 @@
 package com.cooba.sql_generator;
 
 import com.cooba.entity.ChatEntity;
+import com.cooba.entity.ChatRoomEntity;
+import com.cooba.entity.ChatRoomMemberEntity;
 import com.cooba.entity.UserEntity;
 
 public class Main {
     public static void main(String[] args) {
-        TableSqlGenerator.generateSql(UserEntity.class);
+        TableSqlGenerator.generateSql(ChatEntity.class);
     }
 }
